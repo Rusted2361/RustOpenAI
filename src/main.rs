22a6f2e53250e3189334fe7dc3e2a6f25e0fa248
Rusted2361 <hamzasajid4040@gmail.com>
2023-07-20 19:51:51 +0500

    added use statements & struct for request,response,choices

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use hyper::body::Buf;
+use hyper::{header, Body, Client, Request};
+use hyper_tls::HttpsConnector;
+use serde_derive::{Serialize, Deserialize};
+use spinners::{Spinner,Spinners};
+use std::env;
+use std::io::{stdin, stdout, Write};
+
+#[derive(Deserialize, Debug)]
+struct OAIChoices{
+    text: String,
+    index: u8,
+    longprobs: Option<u8>,
+    finish_reason: String
 }
+#[derive(Deserialize, Debug)]
+struct OAIResponse {
+    id: Option<String>,
+    object: Option<String>,
+    created: Option<u64>,
+    model: Option<String>,
+    choices: Option<OAIChoices>
+}
+
+#[derive(Serialize, Debug)]
+struct OAIRequest{
+    prompt: String,
+    max_tokens: u32
+}
+fn main() {}
